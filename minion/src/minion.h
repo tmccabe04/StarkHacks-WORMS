ccacaf2563c61372de3e38d29b9a296fd8b5ecf1
tmccabe04 <mccabe30@purdue.edu>
2026-04-18 05:24:08 -0400

    not working mpu stuff

--- minion/src/minion.h
+++ minion/src/minion.h
@@ -3,7 +3,7 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
-
+#include "MPU9250.h"
 #include <Arduino.h>
-
+#include <HardwareSerial.h>
 void setRGB(int r, int g, int b);
