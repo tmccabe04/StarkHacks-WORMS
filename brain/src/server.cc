9048ccbc43e666ffada6bc155c6b3423c66a9b81
tmccabe04 <mccabe30@purdue.edu>
2026-04-19 05:14:35 -0400

    port corrected

--- brain/src/server.cc
+++ brain/src/server.cc
@@ -7,13 +7,20 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define PORT 10101
+
+void sigchld_handler(int s) {
+    int saved_errno = errno;
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+    errno = saved_errno;
+}
 
 void handleChild(int client_fd) {
     char buffer[1024];
     while (true) {
-        // Receive data from minion
         ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
         if (bytes_read <= 0) {
             if (bytes_read == 0) printf("Minion disconnected\n");
@@ -23,13 +30,12 @@ void handleChild(int client_fd) {
         buffer[bytes_read] = '\0';
         printf("Received data from minion: %s\n", buffer);
 
-        // Send goals to minion
         const char* goal = "GOAL: MOVE_FORWARD";
         if (send(client_fd, goal, strlen(goal), 0) < 0) {
             perror("send error");
             break;
         }
-        sleep(1); // Simulate some processing time
+        sleep(1);
     }
     close(client_fd);
     exit(0);
@@ -40,6 +46,15 @@ void server() {
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
+
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        return;
+    }
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
@@ -60,7 +75,7 @@ void server() {
         return;
     }
 
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 20) < 0) {
         perror("listen");
         return;
     }
@@ -78,12 +93,10 @@ void server() {
 
         pid_t pid = fork();
         if (pid == 0) {
-            // Child process
-            close(server_fd); // Child doesn't need the listener
+            close(server_fd);
             handleChild(client_fd);
         } else if (pid > 0) {
-            // Parent process
-            close(client_fd); // Parent doesn't need the client socket
+            close(client_fd);
         } else {
             perror("fork failed");
             close(client_fd);
